7c7caa91792101684a32181d3409907887547d1f
Vincde <145802178+Vincde@users.noreply.github.com>
2024-01-06 12:48:53 +0100

    minor changes, fixes needed

--- javascript.js
+++ javascript.js
@@ -4,7 +4,27 @@ let loopVariable;
 
 for(let i = 0; i < 16; i++){
     loopVariable = document.createElement('div');
-    loopVariable.className = 'sixteenDiv';
+    loopVariable.className = 'sixteen-div';
     container.appendChild(loopVariable);
 }
 
+const allDivs = document.querySelector('.sixteen-div');
+const changeColor = document.createElement('div');
+changeColor.style.width = 0 + 'px';
+changeColor.style.height = 0 + 'px';            /* this width and height are only the dimensions of the div, not its position in the page */
+                                                // see for position:absolute, maybe u need it
+changeColor.style.border = '5px solid red';    /* red div is visible */
+container.appendChild(changeColor);
+
+
+
+
+allDivs.addEventListener('mouseover',(event) => {                   /* this event is not read by the website. */
+    let x,y;
+    x = event.clientX;
+    y = event.clientY;
+    changeColor.style.width = x + 'px';
+    changeColor.style.height = y + 'px';
+    changeColor.style.backgroundColor= 'red';
+    
+});
