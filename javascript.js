03b35d0624f27904ddc4a2ecbae595a29e0dae45
Vincde <145802178+Vincde@users.noreply.github.com>
2024-01-07 19:51:48 +0100

    added the solution version that needs more things to do

--- javascript.js
+++ javascript.js
@@ -1,26 +1,59 @@
+function createPixel(size){
+    const container = document.querySelector(".container");
 
+    for(currentPixel = 0; currentPixel < size*size; currentPixel++ ){
+        pixel = document.createElement('div');
+        pixel.setAttribute("class", "pixel");
+        pixel.addEventListener("pointerover", addRgb);
 
-function startHere(){                                                                         /* name of the function is to be changed . */
-                                 // this function only works to start the code itself, it's not really the result
-
-createContainer();   //function to create containers
-
-}
-
-
-function createContainer(){
-    /*  let rows; */                 
-                            //  useless?
-    const container = document.querySelector('.big-container'); 
-    let column;
-    for(let i = 0;i < 16; i++){
-        for(let j = 0; j < 16; j++){
-            column = document.createElement('div');
-            column.className = 'squares-dimensions';
-            container.appendChild(column);
-        }
+        container.appendChild(pixel);
+        
     }
-}
-//this code creates all divs without function call. as the function is added to the code they disappear
 
-createContainer();
+}
+
+createPixel(16);
+
+
+
+
+
+function addRgb(e){
+    e.target.style = `background-color: rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+
+    e.target.removeEventListener("pointerover",addRgb);
+
+}
+
+
+
+
+
+
+    
+const b = document.querySelector('.reset-button');
+    b.addEventListener('click',resetColor);
+
+    function resetColor(){
+        const allPixels = document.querySelectorAll(".pixel");
+        allPixels.forEach((pixel) => pixel.style = 'background-color: "white" ');
+    }
+//function resets color.
+
+
+
+
+
+
+
+
+
+//function below is never called
+
+function resetContainer(){
+    const allPixels = document.querySelectorAll(".pixel");
+    allPixels.forEach((pixel) => pixel.parentNode.removeChild(pixel));
+    
+    /* location.reload(); */
+    
+}
