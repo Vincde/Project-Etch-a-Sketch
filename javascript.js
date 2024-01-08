eb961a7f073e629ebcf58a6ef025e4ccc04535b5
Vincde <145802178+Vincde@users.noreply.github.com>
2024-01-08 18:30:06 +0100

    solved the entire thing! now its up to my decision to continue with additional stuff or go ahead

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 function createPixel(size){
     
+    if(size > 100){
+        alert('ATTENTION! you inserted a number > 100 ! you can\'t do that.');
+        return 0;
+    }
+
     const bigContainer = document.querySelector('.big-container');
     let number = (500/(size));
     for(i = 0;i < size * size; i++){
@@ -7,7 +12,7 @@ function createPixel(size){
         pixel.classList.add('pixel-div');
         pixel.style.width = `${number}px`;
         pixel.style.height = `${number}px`;
-        pixel.addEventListener('click',addRgb);
+        pixel.addEventListener('pointerover',addRgb);
             
         bigContainer.appendChild(pixel);
     }
@@ -16,8 +21,24 @@ function createPixel(size){
 
 
 
+function getSize(){
+        const buttonSize = document.querySelector('.button-size');
+        const inputSize = document.querySelector('#input-size');
+        let num;
+        
+    inputSize.addEventListener('input',(e) =>{
+        num = e.target.value;
+    });
+    
 
-createPixel(16);
+    buttonSize.addEventListener('click',function (e){
+        e.preventDefault();
+        createPixel(num);
+    });
+    
+}
+
+getSize();
 
 
 function addRgb(e){
